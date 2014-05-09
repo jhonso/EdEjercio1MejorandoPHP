215f7ece8f24673ed5586404b4e7b67033eadebf
edbror <edbr.or@gmail.com>
2014-05-08 19:40:58 -0700

    version funcional

--- views/contactos.tpl.php
+++ views/contactos.tpl.php
@@ -1,12 +1,2 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Contactos</title>
-</head>
-<body>
-	
-<p>contactos@mejorando.la</p>
 
-</body>
-</html>
+contactos@mejorando.la
--- views/home.tpl.php
+++ views/home.tpl.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title><?= $titulo ?>	</title>
-</head>
-<body>
-	<h1>Hola Mundo!</h1>
-Aprendiendo <strong> <?= $language ?> </strong>
-<p>
-	<a href="contactos">Contactanos</a>
-</p>
 
+<p><?= $titulo ?></p>
 
-</body>
-</html>
+<p><a href="contactos">Contactanos</a></p>
